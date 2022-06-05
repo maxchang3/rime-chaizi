6b5d0aaae6bb10f7598b818fac3863940ac2e5f6
MaxChang3 <maxcfoo@gmail.com>
2022-06-05 12:47:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+<div align="center">
+<img src="preview.png" height="128">
+</div>
+
+# rime-chaizi
+习惯了之前很多输入法的拆词功能，换到 Rime 后还有一点不适应。 搜索得知这一功能的实现往往会通过导入一个拆字词典来解决。但发现这一方案往往会在输入含 u 的拼音的时候出现卡顿。因此这个方案是有其缺陷的。 
+
+根据 @lotem 大佬在这个 [讨论](https://github.com/rime/home/discussions/764) 下的提示，我意识到我们完全可以类似于五笔反查、两分输入来实现上述功能。 因此我修改了[两分输入法](http://cheonhyeong.com/Simplified/download.html) 的方案，并尝试改为了 u 开头。
+
+词典方面，参照 [这位大佬](http://gerry.lamost.org/blog/?p=296003) 构建了拆字词典并做了去重和去除错误字符。
+
+# 使用
+src 下自取 schema 和 dict，也可以自己从 build.py 下构建（需下载 [chaizi-jt.txt](https://github.com/kfcd/chaizi/raw/master/chaizi-jt.txt) ）。
+
+需要配置 u 拆字可以参考下面：（我没有弄好patch）
+```yaml
+  engine/translators:
+     - reverse_lookup_translator
+  dependencies:
+    - chaizi
+  reverse_lookup:
+    dictionary: chaizi
+    enable_completion: true
+    prefix: "u"
+    tips: 〔拆字〕
+    preedit_format:
+      - xform/([nljqxy])v/$1ü/
+    comment_format:
+      - xform/([nljqxy])v/$1ü/
+  recognizer:
+    patterns:
+      reverse_lookup: "u[a-z]*?$"
+```
---
+++ build.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python
+# coding = utf-8
+
+# Modified by Max @ 2022.06.05
+
+# By Gerry @ 2019.12.28
+# Usage: make chaizi dictionary for rime
+# base on :
+# https://github.com/mozillazg/python-pinyin
+# https://github.com/kfcd/chaizi
+
+from pypinyin import lazy_pinyin
+
+
+def chai():
+    # https://github.com/kfcd/chaizi/raw/master/chaizi-jt.txt
+    lines = []
+    with open("chaizi-jt.txt") as f:
+        lines = f.readlines()
+    res = []
+    res.append('''---
+name: chaizi
+version: "2022.06.05"
+sort: by_weight
+use_preset_vocabulary: true
+...''') 
+    for line in lines:
+        data = line.strip().split("\t")
+        if(data[0] == "□"): continue # 去除错误字符
+        for i in range(1, len(data)):
+            py = "".join(lazy_pinyin(data[i].replace(" ", "")))
+            if not py.isalpha():
+                print(data[i])
+                continue
+            item = f"{data[0].strip()}\t{py}"
+            if(item in res): continue # 去重
+            res.append(item)
+    with open("src/chaizi.dict.yaml", "w") as f:
+        f.write("\n".join(res))
+
+
+if __name__ == "__main__":
+    chai()
